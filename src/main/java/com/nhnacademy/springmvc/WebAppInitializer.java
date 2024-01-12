09895f426998363feea488f1a169ea502fa1a4e4
Daeyoul-Yang <cnrrnehfdl03@naver.com>
2024-01-12 12:26:55 +0900

    feat: LoginForm.html

---
+++ src/main/java/com/nhnacademy/springmvc/WebAppInitializer.java
@@ -0,0 +1,21 @@
+package com.nhnacademy.springmvc;
+
+import com.nhnacademy.springmvc.config.RootConfig;
+import com.nhnacademy.springmvc.config.WebConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+
+    @Override
+    protected String[] getServletMappings(){ return  new String[] {"/"};}
+
+    @Override
+    protected Class<?>[] getRootConfigClasses(){ return new Class[] {RootConfig.class};}
+
+    @Override
+    protected Class<?>[] getServletConfigClasses(){ return new Class[] {WebConfig.class};}
+
+
+}
---
+++ src/main/java/com/nhnacademy/springmvc/repository/Base.java
@@ -0,0 +1,5 @@
+package com.nhnacademy.springmvc.repository;
+
+public interface Base {
+
+}
